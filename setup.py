96925a42dde32fcc7c9f175d74cb748b8cf6c307
Matthias <matthias-endler@gmx.net>
2022-09-14 18:56:33 +0200

    Add `long_description`

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     packages=find_packages(include=['spotifyconnector']),
     version='0.1.0',
     description='Spotify Connector for Podcast Data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Open Podcast',
     license='MIT',
     install_requires=[
