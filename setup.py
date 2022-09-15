a556e450f019e46914894f5ec2ee2a5b88b62fd2
Matthias <matthias-endler@gmx.net>
2022-09-15 12:00:18 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='spotifyconnector',
     packages=find_packages(include=['spotifyconnector']),
-    version='0.1.0',
+    version='0.1.1',
     description='Spotify Connector for Podcast Data',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
