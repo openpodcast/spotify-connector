3159a78778081ea0a6b7b3d230ab61762f0a5590
Matthias <matthias@endler.dev>
2024-04-20 18:26:51 +0200

    Expose `CredentialsExpired` so that we can handle it downstream

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="spotifyconnector",
     packages=find_packages(include=["spotifyconnector"]),
-    version="0.8.1",
+    version="0.8.2",
     description="Spotify Connector for Podcast Data",
     long_description=long_description,
     long_description_content_type="text/markdown",
