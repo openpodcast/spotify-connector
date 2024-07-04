e4fe7a983d600a411ecb778bd30724bcc17d70b4
Wolfgang Gassler <wolfgang@gassler.org>
2024-07-05 00:59:58 +0200

    Add shows.py to test conenctor an show all shows of credentials and update Makefile

---
+++ tests/shows.py
@@ -0,0 +1,23 @@
+import os
+from spotifyconnector import SpotifyConnector
+
+base_url = os.environ.get("SPOTIFY_BASE_URL")
+client_id = os.environ.get("SPOTIFY_CLIENT_ID")
+podcast_id = os.environ.get("SPOTIFY_PODCAST_ID")
+sp_dc = os.environ.get("SPOTIFY_SP_DC")
+sp_key = os.environ.get("SPOTIFY_SP_KEY")
+
+connector = SpotifyConnector(
+    base_url,
+    client_id,
+    podcast_id,
+    sp_dc,
+    sp_key,
+)
+
+catalog = connector.catalog()
+
+for show in catalog["shows"]:
+    print(f"Show Name: {show['name']}")
+    print(f"Show ID: {show['id']}")
+    print("\n")
